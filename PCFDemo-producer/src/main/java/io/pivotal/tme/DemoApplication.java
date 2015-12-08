9ebd1ba88cd6ffd907d1f45bab740109de4114af
bbertka <bbertka@gmail.com>
2015-12-08 13:33:29 -0800

    Added service discovery

--- PCFDemo-producer/src/main/java/io/pivotal/tme/DemoApplication.java
+++ PCFDemo-producer/src/main/java/io/pivotal/tme/DemoApplication.java
@@ -2,8 +2,10 @@ package io.pivotal.tme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {	
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
