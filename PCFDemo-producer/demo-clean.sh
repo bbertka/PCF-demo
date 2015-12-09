32188b39ee66192e169c832a686ecec38cb92f38
bbertka <bbertka@gmail.com>
2015-12-09 12:25:18 -0800

    Added correct app names for scripts

--- PCFDemo-producer/demo-clean.sh
+++ PCFDemo-producer/demo-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cf delete "South Customer Region" -f
-cf delete "Customer Region" -f
-cf delete "XFINITY Fantasy Football Pilot" -f
+cf delete "East Customer Region" -f
+cf delete "Fantasy Football Pilot" -f
 curl -k https://comcast.vert.fe.gopivotal.com/clear
